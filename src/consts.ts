5d651ab46c078c96cf6aceba63755acaadbc3f48
lgmys <lgmys@pm.me>
2025-08-08 15:30:40 +0200

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Luke\'s blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const GITHUB = 'https://github.com/lgmys';
+export const X = 'https://x.com/itslukethemaker';
